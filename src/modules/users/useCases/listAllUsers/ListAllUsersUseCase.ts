694373a2c720fd742325c415e87779d0bb47aea9
Mauricio <mauhmg@gmail.com>
2023-01-17 14:24:59 -0300

    Correção de parametro no execute de ListAllUsersUseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,10 +1,14 @@
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+interface IRequest {
+  user_id: string;
+}
+
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute(user_id: string): User[] {
+  execute({ user_id }: IRequest): User[] {
     const user = this.usersRepository.findById(user_id as string);
 
     if (!user.admin) {
