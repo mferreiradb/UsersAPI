8686e47d0aeaef53e44c930e80c50b61338a42ec
Mauricio <mauhmg@gmail.com>
2023-01-18 11:07:26 -0300

    Documentação da rota post /users no Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
+import swaggerUI from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerJSON from "./swagger.json";
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 app.listen(3333, () =>
   console.log("Server is running! Acess: http://localhost:3333")
