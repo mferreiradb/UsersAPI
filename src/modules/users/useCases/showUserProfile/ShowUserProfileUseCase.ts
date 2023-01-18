514dcd1c84bd69857a6ef5d3fa1abf7135490206
Mauricio <mauhmg@gmail.com>
2023-01-17 23:16:52 -0300

    Refatoração no recebimento dos parametros no UseCases

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,7 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    const user = this.usersRepository.findById(user_id as string);
+    const user = this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new Error("User does not exist");
